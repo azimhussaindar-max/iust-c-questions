648d961bc81c99b74e388aa9475609729c41126e
azimhussaindar-max <azimhussaindar@gmail.com>
2025-11-25 00:53:49 +0530

    Create constant.c

---
+++ constant.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  const pi = 3.14159
+  printf("%f\n", pi);
+  return 0;
+}
+// constants in c are variables whose vlaue cannot be changed in a program once declared
+// e.g. pi is a constant and it's value need to be same no matter what
+//and for that we use "const" keyword to declare a constant

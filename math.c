bc5496431f9d4d6be78785ee7cf548000053e4d9
azimhussaindar-max <azimhussaindar@gmail.com>
2025-11-25 01:00:34 +0530

    Create math.c

---
+++ math.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  double x = 9.0;
+  printf("Square root of %f is: %f\n", x, sqrt(x));
+  printf("power (2^3) is: %f\n", pow(2, 3));
+  return 0;
+
+}
+  
